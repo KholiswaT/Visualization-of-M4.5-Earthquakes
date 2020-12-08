3e6a7309ded79706944dbe440c3fb5a16a812e41
tsotetsi <kholiswa.tsotetsi@gmail>
2020-12-07 23:30:04 -0500

    addition of time slider

---
+++ leaflet-step2/static/js/app.js
@@ -0,0 +1,151 @@
+// Create map & boundaries
+var myMap = L.map("map", {
+    center: [20, 0],
+    zoom: 2,
+    minZoom: 2
+  });
+  
+  // Adding a base layer for map
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 10,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+  // Store API in newly created variable
+var earthquake = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
+
+
+var sliderControl = null;
+
+//  Call variable and retrieve magnitude data
+d3.json(earthquake, function(data) {
+    console.log(data)
+  function markerStyle(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 0.75,
+      fillColor: colorMarker(feature.properties.mag),
+      color: "black",
+      radius: markerRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.75
+    };
+  }
+  // Switch colors for increasing level of earthquake magnitude
+    function colorMarker(magnitude) {
+    switch (true) {
+    case magnitude > 7:
+      return "black";
+    case magnitude > 6:
+      return "RebeccaPurple";
+    case magnitude > 5.5:
+      return "MediumOrchid";
+    case magnitude > 5:
+      return "red";
+    case magnitude > 4.5:
+      return "orange";
+    default:
+      return "yellow";
+    }
+  }
+// Set color function to be used for legend
+  function getColors(d) {
+    return d > 7 ? "black":
+    d > 6 ? "RebeccaPurple":
+    d > 5.5 ? "MediumOrchid":
+    d > 5 ? "red":
+    d > 4.5 ? "orange" :
+    d > 0 ?  "yellow":
+    "white"
+    }
+
+  // Create function for radius so that it changes in size based on magnitude
+    function markerRadius(magnitude) {
+        return magnitude * 3;
+  }
+// Define pop-up for each feature
+  function onEachFeature(feature, layer) {
+        layer.bindPopup("<strong>Location: </strong>" + feature.properties.place+ "<hr>" + "<strong> Magnitude: </strong> " + feature.properties.mag 
+        + "<br> <strong> Depth of Earthquake: </strong>"+ feature.geometry.coordinates[2] + " (km)"+"<br> <strong> Date of Earthquake: </strong>" + (new Date(parseInt(feature.properties.time))));
+      };
+
+
+
+   
+    $.getJSON(earthquake, function(json) {
+      var testlayer = L.geoJson(json, {
+
+        pointToLayer: function(feature, latlng) {
+          return L.circleMarker(latlng);
+        },
+        style: markerStyle,
+        onEachFeature: onEachFeature
+      }),
+          sliderControl = L.control.sliderControl({
+              position: "topright",
+              layer: testlayer,
+              range:true,
+              follow: true
+          });
+   
+      //Add slider control to map
+      myMap.addControl(sliderControl);
+
+      sliderControl.options.markers.sort(function(a, b) {
+    return (a.feature.properties.time> b.feature.properties.time);
+});
+      //And initialize the slider
+      sliderControl.startSlider();
+    })
+// Create legend
+    var legend = L.control({
+      position: "bottomright"
+    });
+  
+//Add details to legend
+    legend.onAdd = function() { 
+      var div = L.DomUtil.create("div", "info legend");
+      var labels = ['<strong>Richter Scale</strong>']
+      var scale = [0, 4.5, 5, 5.5, 6, 7];
+
+      for (var i = 0; i < scale.length; i++) {
+        div.innerHTML +=
+            labels.push('<i style= "background:' + getColors(scale[i]+0.5) + '"></i> ' +
+            scale[i] + (scale[i + 1] ? "&ndash;" + scale[i + 1]: "+"));
+      }
+      div.innerHTML = labels.join('<br>');
+      return div;
+    };
+    legend.addTo(myMap);
+  });
+
+
+
+
+//   var testlayer = L.geoJson(camps, {
+//     onEachFeature: function(feature, layer) {
+//         layer.bindPopup(feature.properties.DATE_START);
+//     }
+// });
+
+// var sliderControl = L.control.sliderControl({
+//     position: "topright",
+//     layer: testlayer,
+//     timeAttribute: 'DATE_START'
+// });
+
+// //Make sure to add the slider to the map ;-)
+// map.addControl(sliderControl);
+
+// sliderControl.options.markers.sort(function(a, b) {
+//     return (a.feature.properties.DATE_START > b.feature.properties.DATE_START);
+// });
+
+// //And initialize the slider
+// sliderControl.startSlider();
+
+// $('#slider-timestamp').html(options.markers[ui.value].feature.properties.DATE_START.substr(0, 10));
